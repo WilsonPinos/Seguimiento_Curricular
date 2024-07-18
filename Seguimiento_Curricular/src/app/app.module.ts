219ce302fb8a25c9f6b11ef982da3e2796902277
WilsonPinos04 <wilson.pinos.brummel@gmail.com>
2024-07-18 18:33:22 -0500

    componente roles

--- Seguimiento_Curricular/src/app/app.module.ts
+++ Seguimiento_Curricular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
+import { RolesComponent } from './roles/roles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent
+    AdminComponent,
+    RolesComponent
   ],
   imports: [
     BrowserModule,
