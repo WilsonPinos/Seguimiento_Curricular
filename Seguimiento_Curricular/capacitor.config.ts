eafe33edbcf01c3fcf87653f3aded061c59cbfaa
DavidMunozGarc <jose.munoz.est@tecazuay.edu.ec>
2024-07-25 21:59:21 -0500

    android-commit

---
+++ Seguimiento_Curricular/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',  // Asegúrate de que el appId es el correcto para tu app
+  appName: 'seguimiento-curricular',
+  webDir: 'dist/seguimiento-curricular'
+};
+
+export default config;
