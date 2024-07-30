b7f34bc445959949b835ed60a5d7f6dec049788f
DavidMunozGarc <jose.munoz.est@tecazuay.edu.ec>
2024-07-30 04:15:36 -0500

    android

--- Seguimiento_Curricular/capacitor.config.ts
+++ Seguimiento_Curricular/capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',  // Asegúrate de que el appId es el correcto para tu app
   appName: 'seguimiento-curricular',
-  webDir: 'dist/seguimiento-curricular'
+  webDir: 'dist/seguimiento-curricular/browser',  // Actualizado para reflejar la ubicación correcta
+  bundledWebRuntime: false
 };
 
 export default config;
