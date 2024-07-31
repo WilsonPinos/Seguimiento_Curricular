2e4eec814a4265c9328cd68a5aee3278bffcc557
ReyLuisDavidJordan <luisreyjor12@gmail.com>
2024-07-30 22:25:35 -0500

    LR

--- Seguimiento_Curricular/src/app/usuario-form/usuario-form.component.ts
+++ Seguimiento_Curricular/src/app/usuario-form/usuario-form.component.ts
@@ -4,7 +4,6 @@ import { Usuario } from '../usuario-form/usuario.model';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 
-
 @Component({
   selector: 'app-usuario-form',
   templateUrl: './usuario-form.component.html',
@@ -20,23 +19,29 @@ export class UsuarioFormComponent {
     fecha_nacimiento: new Date(),
     cedula: '',
     rol_id: undefined,
-    contrasena: '' // Inicializa el nuevo campo
+    contrasena: ''
   };
 
-  constructor(private usuarioService: UsuarioService,
-    private router: Router
-  ) { }
+  constructor(private usuarioService: UsuarioService, private router: Router) { }
 
   guardarUsuario(): void {
-    const { rol_id, ...usuarioToSave } = this.usuario;
-    console.log('Datos del usuario antes de enviar:', usuarioToSave);
+    if (!this.isFormComplete()) {
+      this.showAlert('Por favor, complete todos los campos correctamente.');
+      return;
+    }
 
-    this.usuarioService.guardarUsuario(usuarioToSave).subscribe(
+    const errores = this.validateFields();
+    if (errores.length > 0) {
+      this.showAlert(`Errores encontrados:\n${errores.join('\n')}`);
+      return;
+    }
+
+    // Si no hay errores, guardar usuario
+    this.usuarioService.guardarUsuario(this.usuario).subscribe(
       data => {
-        console.log('Usuario guardado:', data);
         Swal.fire({
           title: 'Éxito',
-          text: 'Usuario creado exitosamente',
+          text: 'Usuario registrado exitosamente',
           icon: 'success',
           confirmButtonText: 'Aceptar'
         }).then(() => {
@@ -48,6 +53,44 @@ export class UsuarioFormComponent {
         console.error('Error al guardar usuario:', error);
       }
     );
+  }
+
+  isFormComplete(): boolean {
+    return this.usuario.nombre.trim() !== '' &&
+           this.usuario.apellido.trim() !== '' &&
+           this.usuario.telefono.trim() !== '' &&
+           this.usuario.cedula.trim() !== '' &&
+           this.usuario.email.trim() !== '' &&
+           this.usuario.contrasena.trim() !== '';
+  }
+
+  validateFields(): string[] {
+    const errores: string[] = [];
+    if (!/^[0-9]+$/.test(this.usuario.cedula)) {
+      errores.push('Cédula solo debe contener números.');
+    }
+
+    if (!/^[a-zA-Z]+$/.test(this.usuario.nombre)) {
+      errores.push('Nombre solo debe contener letras.');
+    }
+    if (!/^[a-zA-Z]+$/.test(this.usuario.apellido)) {
+      errores.push('Apellido solo debe contener letras.');
+    }
+    if (!/^[0-9]+$/.test(this.usuario.telefono)) {
+      errores.push('Teléfono solo debe contener números.');
+    }
+    
+
+    return errores;
+  }
+
+  private showAlert(message: string): void {
+    Swal.fire({
+      title: 'Error',
+      text: message,
+      icon: 'error',
+      confirmButtonText: 'Aceptar'
+    });
   }
 
   private resetForm(): void {
